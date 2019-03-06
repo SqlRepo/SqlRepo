ecf81560b1885690437df3dc15216aa3cf7db150
Mike Hanson <mike@contractdeveloper.uk>
2019-03-06 07:34:38 +0000

    Switched everything to .NET Core and added extra demo projects

---
+++ Demos/GettingStarted/GettingStarted.SqlServer.Static/GettingStarted.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using GettingStarted.SqlServer.Shared;
+using SqlRepo.SqlServer.Static;
+
+namespace GettingStarted.SqlServer.Static
+{
+    public class GettingStarted
+    {
+        public void DoIt()
+        {
+            var repository = RepoFactory.Create<ToDo>();
+            var results = repository.Query()
+                                    .Select(e => e.Id, e => e.Task, e => e.CreatedDate)
+                                    .Where(e => e.IsCompleted == false)
+                                    .Go();
+            Console.WriteLine("Id,Task,Completed,Created");
+            foreach(var row in results)
+            {
+                Console.WriteLine($"{row.Id},{row.Task},{row.IsCompleted},{row.CreatedDate}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
