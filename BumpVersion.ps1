d79442cc25e30873dfec87423108f5010593f9e0
Mike Hanson <mike@contractdeveloper.uk>
2019-03-07 12:57:56 +0000

    Added separate script to patch project files, removed patching from bump script

--- BumpVersion.ps1
+++ BumpVersion.ps1
@@ -6,14 +6,5 @@ $versionElements = $currentVersion.Split('.')
 $versionElements[2] = [int]$versionElements[2] + 1
 $newVersion = $versionElements -join "."
 
-# Get list of .csproj file paths that contain a version tag
-$files = Get-ChildItem -Recurse "*.csproj" | Where-Object { (Select-String -InputObject $_ -Pattern '<Version>' -Quiet) -eq $true } | Select -expand FullName
-
-# Replace version tag with new version
-foreach ($file in $files)
-{
-	(Get-Content -Path $file) -replace '<Version>.*<\/Version>', "<Version>$newVersion</Version>" | Set-Content $file 
-}
-
 # Update version file
 Set-Content -Path $versionFileName -Value $newVersion
