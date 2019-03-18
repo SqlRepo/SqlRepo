485414f6089d212d4e6e26309e04277e2e5c3cf5
Mike Hanson <mike@contractdeveloper.uk>
2019-03-18 07:04:02 +0000

    Refactored mapping instructions creation into method

--- SqlRepo/DataReaderEntityMapper.cs
+++ SqlRepo/DataReaderEntityMapper.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq.Expressions;
 using System.Reflection;
 using SqlRepo.Abstractions;
 
@@ -10,13 +9,12 @@ namespace SqlRepo
     public class DataReaderEntityMapper : IEntityMapper
     {
         private readonly IEntityMapperDefinitionProvider entityMapperDefinitionProvider;
-        private static readonly Dictionary<string, object> EntityMapperDefinitions =
-            new Dictionary<string, object>();
 
         public DataReaderEntityMapper(IEntityMapperDefinitionProvider entityMapperDefinitionProvider)
         {
             this.entityMapperDefinitionProvider = entityMapperDefinitionProvider;
         }
+
         public virtual IEnumerable<TEntity> Map<TEntity>(IDataReader reader)
             where TEntity: class, new()
         {
@@ -49,7 +47,8 @@ namespace SqlRepo
             return list;
         }
 
-        private void PopulateMappingInstructions<TEntity>(IList<Func<IDataReader, TEntity, TEntity>> mappingInstructions,
+        private void PopulateMappingInstructions<TEntity>(
+            IList<Func<IDataReader, TEntity, TEntity>> mappingInstructions,
             IDataRecord record,
             EntityMapperDefinition<TEntity> entityMapper)
             where TEntity: class, new()
@@ -73,230 +72,230 @@ namespace SqlRepo
 
                     if(dataType == typeof(decimal))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetDecimal(fieldIndex));
+                                                     propertySetter(e, r.GetDecimal(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(decimal?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetDecimal(fieldIndex));
+                                                     propertySetter(e, r.GetDecimal(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(string))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetString(fieldIndex));
+                                                     propertySetter(e, r.GetString(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(short))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetInt16(fieldIndex));
+                                                     propertySetter(e, r.GetInt16(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(short?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetInt16(fieldIndex));
+                                                     propertySetter(e, r.GetInt16(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(int))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetInt32(fieldIndex));
+                                                     propertySetter(e, r.GetInt32(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(int?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetInt32(fieldIndex));
+                                                     propertySetter(e, r.GetInt32(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(long))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetInt64(fieldIndex));
+                                                     propertySetter(e, r.GetInt64(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(long?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetInt64(fieldIndex));
+                                                     propertySetter(e, r.GetInt64(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(DateTime))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetDateTime(fieldIndex));
+                                                     propertySetter(e, r.GetDateTime(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(DateTime?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetDateTime(fieldIndex));
+                                                     propertySetter(e, r.GetDateTime(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(double))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetDouble(fieldIndex));
+                                                     propertySetter(e, r.GetDouble(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(double?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetDouble(fieldIndex));
+                                                     propertySetter(e, r.GetDouble(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(bool))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetBoolean(fieldIndex));
+                                                     propertySetter(e, r.GetBoolean(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(bool?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetBoolean(fieldIndex));
+                                                     propertySetter(e, r.GetBoolean(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(byte))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetByte(fieldIndex));
+                                                     propertySetter(e, r.GetByte(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(byte?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetByte(fieldIndex));
+                                                     propertySetter(e, r.GetByte(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(float))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     propertySetter(e, record.GetFloat(fieldIndex));
+                                                     propertySetter(e, r.GetFloat(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else if(dataType == typeof(float?))
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetFloat(fieldIndex));
+                                                     propertySetter(e, r.GetFloat(fieldIndex));
                                                      return e;
                                                  };
                     }
                     else
                     {
-                        mappingInstructions[i] = (dataReader, e) =>
+                        mappingInstructions[i] = (r, e) =>
                                                  {
-                                                     if(record.IsDBNull(fieldIndex))
+                                                     if(r.IsDBNull(fieldIndex))
                                                      {
                                                          return e;
                                                      }
 
-                                                     propertySetter(e, record.GetValue(fieldIndex));
+                                                     propertySetter(e, r.GetValue(fieldIndex));
                                                      return e;
                                                  };
                     }
                 }
                 else
                 {
-                    mappingInstructions[i] = (dataReader, e) =>
+                    mappingInstructions[i] = (r, e) =>
                                              {
-                                                 if(record.IsDBNull(fieldIndex))
+                                                 if(r.IsDBNull(fieldIndex))
                                                  {
                                                      return e;
                                                  }
 
-                                                 propertySetter(e, record.GetValue(fieldIndex));
+                                                 propertySetter(e, r.GetValue(fieldIndex));
                                                  return e;
                                              };
                 }
